95a341889aed180e28ca91610db1ffe6fae477db
Sppy A.R <developer10@shareppy.com>
2022-07-01 16:09:10 -0500

    proyecto frondend

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import "@fortawesome/fontawesome-free/js/all";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Clientes from "./pages/Clientes";
 import Home from "./pages/Home";
+import Axios from "axios";
 
+Axios.interceptors.request.use(function (config) {
+  config.url = `${process.env.REACT_APP_API_BASE_URL}${config.url}`;
+  return  config;}
+);
 
 const App = () => {
   return (
--- src/contexts/clienteContext.js
+++ src/contexts/clienteContext.js
@@ -1,82 +1,151 @@
 import React, { createContext, useReducer } from "react";
-import { OBTENER_CLIENTES, REGISTRAR_CLIENTE,OBTENER_CLIENTE, MODIFICAR_CLIENTE,ELIMINAR_CLIENTE } from "../const/actionTypes";
+import {
+  OBTENER_CLIENTES,
+  REGISTRAR_CLIENTE,
+  OBTENER_CLIENTE,
+  MODIFICAR_CLIENTE,
+  ELIMINAR_CLIENTE,
+} from "../const/actionTypes";
 import ClienteRedurcer from "../reducer/clienteReducer";
-import {v4 as uuidv4} from 'uuid';
+import Axios from "axios";
+import Swal from 'sweetalert2';
 
 export const ClienteContext = createContext();
 
 export const ClienteContextProvider = (props) => {
   const initialState = {
     clientesList: [],
-    clienteActual: null
+    clienteActual: null,
   };
 
   //dispatch permite enviar una acción
   const [state, dispatch] = useReducer(ClienteRedurcer, initialState);
 
-  const obtenerClientes= () =>{
-    const clientes =[
-      {
-        idCliente: 1,
-        nombres: "Mario",
-        apellidos: "andres",
-        direccion: "centro",
-        telefono: "31256987",
-        email: "mario@gmail.com",
-      },
-      {
-        idCliente: 2,
-        nombres: "Julian",
-        apellidos: "ser",
-        direccion: "av. 100",
-        telefono: "30345434",
-        email: "julain@gmail.com",
-      },
-    ];
-
-    dispatch({
-      type: OBTENER_CLIENTES,
-      payload: clientes
-    })
-  }
-
-  const registrarCliente = cliente=>{
-
-    let clienteNuevo={
-      ...cliente,
-      idCliente: uuidv4()
+  const obtenerClientes = async () => {
+    try {
+      const resultado = await Axios.get("/clientes");
+      dispatch({
+        type: OBTENER_CLIENTES,
+        payload: resultado.data,
+      });
+    } catch (error) {
+      Swal.fire({
+        icon:'error',
+        title:'Error',
+        text: 'No se logró obtener los clientes',
+        toast: true
+      });
+      console.log(error);
     }
+  };
 
-    console.log(clienteNuevo);
-    dispatch({
-      type: REGISTRAR_CLIENTE,
-      payload: clienteNuevo,
+  const registrarCliente = async (cliente) => {
+    try {
+      const resultado = await Axios.post("/clientes", cliente);
+      dispatch({
+        type: REGISTRAR_CLIENTE,
+        payload: resultado.data,
+      });
+      Swal.fire({
+        icon:'success',
+        title:'Correcto',
+        text: 'Cliente registrado',
+        toast: true
+      });
+    } catch (error) {
+      Swal.fire({
+        icon:'error',
+        title:'Error',
+        text: 'No se pudo registrar el cliente',
+        toast: true
+      });
+      console.log(error);
+    }
+  };
+
+  const obtenerCliente = async (cliente) => {
+    try {
+      let clienteEncontrado = null;
+      if (cliente !== null) {
+        const resultado = await Axios.get(`/clientes/${cliente.idCliente}`);
+        clienteEncontrado = resultado.data;
+      } else {
+        clienteEncontrado = cliente;
+      }
+      dispatch({
+        type: OBTENER_CLIENTE,
+        payload: clienteEncontrado,
+      });
+    } catch (error) {
+      Swal.fire({
+        icon:'error',
+        title:'Error',
+        text: 'No se logró obtener el cliente',
+        toast: true
+      });
+      console.log(error);
+    }
+  };
+
+  const actualizarCliente = async (cliente) => {
+    try {
+      const resultado = await Axios.put(`/clientes`, cliente);
+      dispatch({
+        type: MODIFICAR_CLIENTE,
+        payload: resultado.data,
+      });
+      Swal.fire({
+        icon:'success',
+        title:'Correcto',
+        text: 'Cliente modificado',
+        toast: true
+      });
+    } catch (error) {
+      Swal.fire({
+        icon:'error',
+        title:'Error',
+        text: 'No se logró actualizar el cliente',
+        toast: true
+      });
+      console.log(error);
+    }
+  };
+
+  const eliminarCliente = async (idCliente) => {
+    try {
+
+      Swal.fire({
+        title: '¿Desea continuar?',
+        text: 'Se eliminará el cliente seleccionado',
+        icon:'question',
+        showCancelButton: true,
+        confirmButtonText:'Si, Eliminar',
+      }).then(async (result)=>{
+        if(result.value){
+          await Axios.delete(`/clientes/${idCliente}`);
+      dispatch({
+        type: ELIMINAR_CLIENTE,
+        payload: idCliente,
+      });
+      Swal.fire({
+        icon:'success',
+        title:'Correcto',
+        text: 'Cliente eliminado',
+        toast: true
+      });
+        }
+      })
       
-    })
-  }
-
-  const obtenerCliente=cliente=>{
-    dispatch({
-
-      type: OBTENER_CLIENTE,
-      payload: cliente
-
-    })
-  }
-
-  const actualizarCliente = cliente=>{
-    dispatch({
-      type: MODIFICAR_CLIENTE,
-      payload:cliente
-    })
-  }
-
-  const eliminarCliente= idCliente=>{
-    dispatch({
-      type: ELIMINAR_CLIENTE,
-      payload: idCliente
-    })
-  }
+    } catch (error) {
+      Swal.fire({
+        icon:'error',
+        title:'Error',
+        text: 'No se logró eliminar el cliente',
+        toast: true
+      });
+      console.log(error);
+    }
+  };
 
   return (
     <ClienteContext.Provider
@@ -88,10 +157,10 @@ export const ClienteContextProvider = (props) => {
         registrarCliente,
         obtenerCliente,
         actualizarCliente,
-        eliminarCliente
+        eliminarCliente,
       }}
     >
       {props.children}
     </ClienteContext.Provider>
-  )
-}
+  );
+};
